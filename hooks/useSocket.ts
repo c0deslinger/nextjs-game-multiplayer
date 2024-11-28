473300c6d1689fd111fee9a25329f27d31bff057
Mochammad Yusuf Fachroni <yusuf.fachroni@gmail.com>
2024-11-28 08:55:48 +0700

    update socket

--- hooks/useSocket.ts
+++ hooks/useSocket.ts
@@ -2,7 +2,7 @@ import { useEffect, useRef } from 'react';
 import { io, Socket } from 'socket.io-client';
 
 // const SOCKET_SERVER_URL = 'http://localhost:3000'; // Ganti jika berbeda
-const SOCKET_SERVER_URL = 'https://bb95-202-51-121-109.ngrok-free.app'; // Ganti jika berbeda
+const SOCKET_SERVER_URL = 'https://a48e-2404-c0-9ca0-00-22d1-7bd5.ngrok-free.app'; // Ganti jika berbeda
 
 export const useSocket = () => {
   const socketRef = useRef<Socket | null>(null);
